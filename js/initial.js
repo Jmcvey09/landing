94d5680680a8aa69bb58850b9df3c0e492a05bd4
Jose <jmcvey09@outlook.com>
2020-07-21 00:51:11 -0700

    updated typewriter

--- js/initial.js
+++ js/initial.js
@@ -22,22 +22,160 @@ setInterval(function () {
 
 //Typewriter Text
 
-var messages=["HTML","CSS","JavaScript"];
-var rank=0;
-
-// Code for Chrome, Safari and Opera
-document.getElementById("myTypewriter").addEventListener("webkitAnimationEnd", changeTxt);
-
-// Standard syntax
-document.getElementById("myTypewriter").addEventListener("animationend", changeTxt);
-
-function changeTxt(e){
-  _h2 = this.getElementsByTagName("h2")[0];
-  _h2.style.webkitAnimation = 'none'; // set element animation to none
-   setTimeout(function() { // you surely want a delay before the next message appears
-      _h2.innerHTML=messages[rank];
-      var speed =5*messages[rank].length/20; // adjust the speed (3.5 is the original speed, 20 is the original string length
-      _h2.style.webkitAnimation = 'typing '+speed+'s steps(40, end), blink-caret .75s step-end infinite'; //  switch to the original set of animation      
-      (rank===messages.length-1)?rank=0:rank++; // if you have displayed the last message from the array, go back to the first one, else go to next message
-    }, 1000);
-}
+var typed = new Typed('.typewrap1 h2 .typewrap', {
+    /**
+     * @property {array} strings strings to be typed
+     * @property {string} stringsElement ID of element containing string children
+     */
+    strings: [
+      'Front-end Developer',
+      'Front-end Developer',
+      'HTML',
+      'CSS',
+      'JavaScript'
+    ],
+    stringsElement: null,
+  
+    /**
+     * @property {number} typeSpeed type speed in milliseconds
+     */
+    typeSpeed: 350,
+  
+    /**
+     * @property {number} startDelay time before typing starts in milliseconds
+     */
+    startDelay: 0,
+  
+    /**
+     * @property {number} backSpeed backspacing speed in milliseconds
+     */
+    backSpeed: 350,
+  
+    /**
+     * @property {boolean} smartBackspace only backspace what doesn't match the previous string
+     */
+    smartBackspace: true,
+  
+    /**
+     * @property {boolean} shuffle shuffle the strings
+     */
+    shuffle: false,
+  
+    /**
+     * @property {number} backDelay time before backspacing in milliseconds
+     */
+    backDelay: 200,
+  
+    /**
+     * @property {boolean} fadeOut Fade out instead of backspace
+     * @property {string} fadeOutClass css class for fade animation
+     * @property {boolean} fadeOutDelay Fade out delay in milliseconds
+     */
+    fadeOut: false,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 500,
+  
+    /**
+     * @property {boolean} loop loop strings
+     * @property {number} loopCount amount of loops
+     */
+    loop: true,
+    loopCount: Infinity,
+  
+    /**
+     * @property {boolean} showCursor show cursor
+     * @property {string} cursorChar character for cursor
+     * @property {boolean} autoInsertCss insert CSS for cursor and fadeOut into HTML <head>
+     */
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: false,
+  
+    /**
+     * @property {string} attr attribute for typing
+     * Ex: input placeholder, value, or just HTML text
+     */
+    attr: null,
+  
+    /**
+     * @property {boolean} bindInputFocusEvents bind to focus and blur if el is text input
+     */
+    bindInputFocusEvents: false,
+  
+    /**
+     * @property {string} contentType 'html' or 'null' for plaintext
+     */
+    contentType: 'html',
+  
+    /**
+     * Before it begins typing
+     * @param {Typed} self
+     */
+    onBegin: (self) => {},
+  
+    /**
+     * All typing is complete
+     * @param {Typed} self
+     */
+    onComplete: (self) => {},
+  
+    /**
+     * Before each string is typed
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    preStringTyped: (arrayPos, self) => {},
+  
+    /**
+     * After each string is typed
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    onStringTyped: (arrayPos, self) => {},
+  
+    /**
+     * During looping, after last string is typed
+     * @param {Typed} self
+     */
+    onLastStringBackspaced: (self) => {},
+  
+    /**
+     * Typing has been stopped
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    onTypingPaused: (arrayPos, self) => {},
+  
+    /**
+     * Typing has been started after being stopped
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    onTypingResumed: (arrayPos, self) => {},
+  
+    /**
+     * After reset
+     * @param {Typed} self
+     */
+    onReset: (self) => {},
+  
+    /**
+     * After stop
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    onStop: (arrayPos, self) => {},
+  
+    /**
+     * After start
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    onStart: (arrayPos, self) => {},
+  
+    /**
+     * After destroy
+     * @param {Typed} self
+     */
+    onDestroy: (self) => {}
+  });
